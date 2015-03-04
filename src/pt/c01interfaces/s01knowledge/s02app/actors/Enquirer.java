2e1b038f8fa13593a50c8ab29784218db2dff1f5
henriquefacioli <henrique.facioli@gmail.com>
2015-03-04 20:06:03 -0300

    Checagem de todos os arquivos do bd
    Programa já pega todos os arquivos texto do bd e já os percorre,
    procurando o correto.

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -10,35 +10,53 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 public class Enquirer implements IEnquirer
 {
     IObjetoConhecimento obj;
-	
+
 	public Enquirer()
 	{
 	}
-	
-	
+
+
 	@Override
 	public void connect(IResponder responder)
 	{
-        IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
+        int animal = 0;
+        boolean animalEsperado = true, achou = false;
 
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
+        IBaseConhecimento bc = new BaseConhecimento();
+        /*Armazena em um array de string a lista de animais*/
+        String listaAnimais[] = bc.listaNomes();
+        /*Faz do objeto o primeiro animal da string de animais*/
+
+        while(!achou){
+            /*Faz do objeto o animal da listaAnimal na posicao animal.
+              Ex. 0 = aranha., 1 = camarao...*/
+            obj = bc.recuperaObjeto(listaAnimais[animal]);
+            IDeclaracao decl = obj.primeira();
+
+            while(decl != null && animalEsperado) {
+                /*Pega a pergunta e a resposta já dentro do bd*/
+                String pergunta = decl.getPropriedade();
+                String respostaEsperada = decl.getValor();
+
+                String resposta = responder.ask(pergunta);
+                if (resposta.equalsIgnoreCase(respostaEsperada))
+                    decl = obj.proxima();
+                else
+                    animalEsperado = false;
+            }
+
+            /*Caso saiu do loop acima sem ser o animal esperado, ele incrementa
+            o contador do animal e faz novamente o loop*/
+            if(!animalEsperado){
+                animal++;
+                animalEsperado = true;
+            }else{
+                achou = true;
+            }
+        }
+
+        boolean acertei = responder.finalAnswer(listaAnimais[animal]);
+
 		if (acertei)
 			System.out.println("Oba! Acertei!");
 		else
